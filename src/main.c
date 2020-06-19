7db9342fd9e3ba584a86e11614077d8242010abb
Joan Lima <joanlima95@gmail.com>
2020-06-19 11:48:30 -0300

    Prime number complete

--- src/main.c
+++ src/main.c
@@ -15,11 +15,13 @@
 #define QNTD_PROCESSOS 4
 #define N_NUMBERS 64
 
+
 int main() {
 
    pid_t filho[QNTD_PROCESSOS];	
    int qntd_divisores=0, numeros = 0, cont = 0,i =0;
    int c; //Variável que auxilia na leitura dos números
+   unsigned int j = 0;
    unsigned long int numb[N_NUMBERS]; //Vetor que armazena os números que serão analisados
 
    for(cont=0;cont<N_NUMBERS;cont++) //Laço que inicializa o vetor com o caracter '\0' para não ler lixo
@@ -32,9 +34,9 @@ int main() {
       c = scanf("%ld",&numb[numeros]);
       numeros++;
    } 
-   
-/*
-   //Processos filhos
+ 
+
+/*   //Processos filhos
    for (cont = 0; cont < QNTD_PROCESSOS; cont++)
    {
       filho[cont] = fork();
@@ -43,11 +45,32 @@ int main() {
       {
          printf("Estou no processo filho %d\n", cont);
          exit(0);
+      } else 
+      {
+         printf("O PID do processo filho %d é %d,",i,filho[i]);
       }    
-   }*/
-   for (i = 0; i < numeros-1; i++)
+   }
+*/
+   for (i = 0; i < (numeros-1); i++) //Laço que guarda os números de entrada. 
    {
-      printf("Os números inseridos são %ld\n",numb[i]);
+      for (j = 2; j <= numb[i]/2; j++) //Laço que verifica se cada número é primo.
+      {
+         if (numb[i]%j==0)
+         {
+            qntd_divisores++;
+            break;
+         }  
+      }
+      if (qntd_divisores==0)
+         {
+            printf("O número %ld é primo\n", numb[i]);
+         } else
+         {
+            printf("O número %ld não é primo\n", numb [i]);
+            qntd_divisores = 0;      
+         }
+      
+      //printf("Os números inseridos são %ld\n",numb[i]);
    }
    
 
