d1e6aa0c827f64b909eafaa8fc1899b10595dd67
Joan Lima <joanlima95@gmail.com>
2020-06-20 00:22:45 -0300

    Final program

--- src/main.c
+++ src/main.c
@@ -18,15 +18,9 @@
 unsigned long int ver_primo(unsigned long int Num) //Essa função retorna o número 1 se o número é primo, e 0 caso contrário
 {
    int qntd_divisores = 0, div = 0;
-   if (Num==0)
-   {
-      qntd_divisores = 0;
-      printf("O número %ld não é primo\n",Num);
-   } else if (Num==1)
-   {
-      qntd_divisores = 0;
-      printf("O número %ld não é primo\n", Num);
-   } else 
+   if (Num==0) qntd_divisores = 0;
+   else if (Num==1) qntd_divisores = 0;
+   else 
    {
       for (unsigned int j = 2; j <= Num/2; j++) //Laço que verifica se cada número é primo.
       {
@@ -38,11 +32,10 @@ unsigned long int ver_primo(unsigned long int Num) //Essa função retorna o nú
       }
       if (qntd_divisores==0)
          {
-            printf("O número %ld é primo\n", Num);
             div++;
          } else
          {
-            printf("O número %ld não é primo\n", Num);
+            //printf("O número %ld não é primo\n", Num);
             qntd_divisores = 0;      
          }
    } 
@@ -52,9 +45,10 @@ unsigned long int ver_primo(unsigned long int Num) //Essa função retorna o nú
 int main() {
 
    pid_t filho[QNTD_PROCESSOS];	
-   int numeros = 0, cont = 0, j =0; //Variáveis auxiliares
+   int numeros = 0, cont = 0, j =0,qntd_primos=0; //Variáveis auxiliares
    char c; //Variável que auxilia na leitura dos números
-   unsigned long int numb[N_NUMBERS]; //Vetor que armazena os números que serão analisados
+   unsigned int numb[N_NUMBERS]; //Vetor que armazena os números que serão analisados
+
    //Definição de flags para proteção e visibilidade de memória 
    int protection = PROT_READ | PROT_WRITE ;
    int visibility = MAP_SHARED | MAP_ANON ;
@@ -62,41 +56,36 @@ int main() {
    int *numPrimos; // Ponteiro que será o vetor de armazenamento se há ou não números primos
    numPrimos = (int *) mmap(NULL,sizeof(int)*64,protection,visibility,0,0); 
 
-
    for(cont=0;cont<N_NUMBERS;cont++) //Laço que inicializa o vetor com o caracter '\0' para não ler lixo
    {
       numb[cont] = '\0';
       numPrimos[cont] = '\0';
    }
-   
     //Laço de leitura e armazenamento da entrada
    while ((c!='\n') && (c!=EOF)) 
    {
-      c = scanf("%ld",&numb[numeros]);
+      c = scanf("%u",&numb[numeros]);
       numeros++;
    } 
-   
-   for (j=0; j < (numeros - 1); j++)
-   {
-      ver_primo(numb[j]);
-   }
-   
-   
-/*   //Processos filhos
+   //Processos paralelos que armazenam a quantidade de números primos
    for (cont = 0; cont < QNTD_PROCESSOS; cont++)
    {
       filho[cont] = fork();
-
       if (filho[cont] ==0)
-      {
-         printf("Estou no processo filho %d\n", cont);
-         exit(0);
-      } else 
-      {
-         printf("O PID do processo filho %d é %d,",i,filho[i]);
-      }    
+      {   
+         for(int k = 0; cont+k < numeros-1; k += QNTD_PROCESSOS)
+         {
+            numPrimos[cont] +=ver_primo(numb[cont+k]);
+         }
+         exit(0);       
+      }  
    }
-*/
+   //Laço que finaliza os processos iniciados
+   for(cont=0;cont<QNTD_PROCESSOS;cont++) waitpid(filho[cont],NULL,0);
+  
+   for (j = 0; j < numeros-1; j++) qntd_primos+=numPrimos[j];
+    
+   printf("%d\n",qntd_primos);
    
   return 0;
 }
